59a578a03512829788b3b98923865c91553c4f87
corre <corre@xTorresx-desktop>
2021-11-28 17:44:59 -0700

    update

---
+++ src/ConexionDB.java
@@ -0,0 +1,30 @@
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author corre
+ */
+public class ConexionDB {
+           
+    ConnectionString connectionString = new ConnectionString("mongodb+srv://TorresAdmin:Torres1@cluster0.jmlta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+    MongoClientSettings settings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .build();
+    MongoClient mongoClient = MongoClients.create(settings);
+    MongoDatabase database = mongoClient.getDatabase("TAP_Practica2_MongoDBAtlas");
+
+    public MongoDatabase obtenerDB(){
+        return database;
+    }
+    
+}
